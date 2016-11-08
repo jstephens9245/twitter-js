f08498bfb36d2e8cea2ba2ff4b992472b4b4afb5
Joe Stephens <Joe Stephens>
2016-11-08 17:13:11 -0600

    finished with the day, ended with creating a route for tweet link

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const chalk = require('chalk');
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const routes = require('./routes/');
 
 const app = express();
-
-let people = [
-    { name: 'Joe Stephens' },
-    { name: 'Tom' }
-
-  ];
 
 app.engine('html', nunjucks.render)
 app.set('view engine', 'html');
@@ -22,25 +15,5 @@ app.use(express.static('public'));
 app.use('/', routes);
 
 
-app.use(function(resquest, response, next) {
-  console.log(chalk.blue("is anyone there?"));
-  next();
-})
-app.use('/special/' , function(resquest, response, next) {
-  console.log(chalk.red("we've reached the special area yayayayayayay"));
-  next();
-})
 
-// app.get("/", function(request, response) {
-//   response.render('index', { title: "Welcome to the twitter", people: people })
-// } )
-app.get("/special", function(request, response) {
-  response.send("Welcome to the special page, just for you, you special boy you");
-})
-app.get("/special/person", function(request, response) {
-  response.send("Welcome special boy");
-})
-app.get("/news", function(request, response) {
-  response.send("Welcome to the news page");
-})
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,6 @@ router.get('/stylesheets/style.css', function(req, res) {
   res.sendFile('public/stylesheets/style.css');
 })
 
-router.use(function(resquest, response, next) {
-  // console.log(chalk.blue("is anyone there?"));
-  next();
-})
-
-router.use('/special/' , function(resquest, response, next) {
-  // console.log(chalk.red("we've reached the special area yayayayayayay"));
-  next();
-})
-
 router.get("/special", function(request, response) {
   response.send("Welcome to the special page, just for you, you special boy you");
 })
@@ -34,6 +24,23 @@ router.get("/special/person", function(request, response) {
 
 router.get("/news", function(request, response) {
   response.send("Welcome to the news page");
+})
+// router.get( '/users/:name', function (req, res) {
+//   res.send( req.params.name ); // --> 'nimit'
+// });
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  // console.log(list);
+  res.render( 'index', { tweets: list } );
+});
+
+router.get('/tweets/:id', function(req, res) {
+  var tweetID = req.params.id
+  console.log(tweetID);
+  var oneTweet = tweetBank.find({id: Number(tweetID)});
+  res.render('index', {tweets: oneTweet});
 })
 
 module.exports = router;
